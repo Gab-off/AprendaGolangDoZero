6c93458027a9c794e58e6abadd326f68ed512c4c
Gabriel de Miranda Albuquerque <albuquerque1.gabriel@gmail.com>
2023-05-29 23:17:38 -0300

    loops for i

--- 13 - Loops/loops.go
+++ 13 - Loops/loops.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
-	
+	i := 0
+
+	for i < 10 {
+		i++
+		fmt.Println("incrementando i")
+		time.Sleep(time.Second)
+	}
+
+	fmt.Println(i)
+
 }
