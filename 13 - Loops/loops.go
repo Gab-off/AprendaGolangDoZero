491bce9f6d2fdc27b017068c8916ddf37c299471
gab-off <albuquerque1.gabriel@gmail.com>
2023-06-07 09:17:11 -0300

    panic-recover finished

--- 13 - Loops/loops.go
+++ 13 - Loops/loops.go
@@ -2,18 +2,30 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	i := 0
+	// i := 0
 
-	for i < 10 {
-		i++
-		fmt.Println("incrementando i")
-		time.Sleep(time.Second)
+	// for i < 10 {
+	// 	i++
+	// 	fmt.Println("incrementando i")
+	// 	time.Sleep(time.Second)
+	// }
+
+	// for j := 0; j < 10; j++ {
+	// 	fmt.Println("Incrementando j", j)
+	// 	time.Sleep(time.Second)
+	// }
+
+	// nomes := [3]string{"João", "Davi", "Lucas"}
+
+	// for _, nome := range nomes {
+	// 	fmt.Println(nome)
+	// }
+
+	for indice, letra := range "PALAVRA" {
+		fmt.Println(indice, string(letra))
 	}
-
-	fmt.Println(i)
 
 }
--- 14 - Funções Avançadas/14.3 - Funções anonimas/anonimas.go
+++ 14 - Funções Avançadas/14.3 - Funções anonimas/anonimas.go
@@ -9,5 +9,4 @@ func main() {
 	}("universo")
 
 	fmt.Println(retorno)
-
 }
